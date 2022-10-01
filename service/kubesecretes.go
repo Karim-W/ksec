f7d55b56bae89b5a87ea8fd09830b678201c2992
Karim <karim.wael@gmail.com>
2022-10-01 16:19:59 +0400

    added secret functionality

--- service/kubesecretes.go
+++ service/kubesecretes.go
@@ -6,7 +6,9 @@ import (
 	"sync"
 
 	"github.com/karim-w/ksec/models"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// v1 "k8s.io/client-go/applyconfigurations/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -35,7 +37,7 @@ func GetKubeClient() *kubernetes.Clientset {
 
 func KubectlSecretsSvc(conf *models.Secrets) {
 	if conf.Set {
-		fmt.Println("Set")
+		addKubeSecret(conf.Namespace, conf.Secret, conf.Key, conf.Value)
 		return
 	}
 	if conf.Get {
@@ -53,7 +55,7 @@ func KubectlSecretsSvc(conf *models.Secrets) {
 }
 
 func getkubeSecretValue(namespace string, secret string, key string) {
-	s, err := GetKubeClient().CoreV1().Secrets(namespace).Get(context.TODO(), secret, v1.GetOptions{})
+	s, err := GetKubeClient().CoreV1().Secrets(namespace).Get(context.TODO(), secret, metav1.GetOptions{})
 	if err != nil {
 		println(err.Error())
 		return
@@ -62,7 +64,26 @@ func getkubeSecretValue(namespace string, secret string, key string) {
 }
 
 func listKubeSecrets(namespace string, secret string) {
-	s, err := GetKubeClient().CoreV1().Secrets(namespace).Get(context.TODO(), secret, v1.GetOptions{})
+	s, err := GetKubeClient().CoreV1().Secrets(namespace).Get(context.TODO(), secret, metav1.GetOptions{})
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	for k, v := range s.Data {
+		fmt.Println(k, string(v))
+	}
+}
+
+func addKubeSecret(namespace string, secret string, key string, value string) {
+	//get secrets
+	s, err := GetKubeClient().CoreV1().Secrets(namespace).Get(context.TODO(), secret, metav1.GetOptions{})
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	s.Data[key] = []byte(value)
+	//update secret
+	s, err = GetKubeClient().CoreV1().Secrets(namespace).Update(context.TODO(), s, metav1.UpdateOptions{})
 	if err != nil {
 		println(err.Error())
 		return
